5d7da9fd98a541d200c6d165e41355b2eb73d744
GabrielBelo01 <Gabriel_itu2013@hotmail.com>
2025-09-18 21:07:00 -0300

    Terceiro Commit

---
+++ phpinfo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <body>
+            <?php
+          
+                /*phpinfo();*/
+
+                echo "Numero inteiros <br>";
+                echo 1;
+                echo "<br>";
+                echo "<br> Numero decimais <br>";
+                echo 2.3;
+                echo "<br>";
+                echo "<br> String <br>";
+                echo "Isso é uma string";
+                echo "<br>";
+                echo "<br>";
+                var_dump(value: 1);
+                echo "<br>";
+                echo "<br>";
+                var_dump(value: 2.1);
+                echo "<br>";
+                echo "<br>";
+                var_dump(value: "Isso é uma string");
+                echo "<br>";
+                echo "<br>";
+            ?>
+        </body>
+    </head>
+</html>    
