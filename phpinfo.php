3dbaad6a9646696ecc5bf1f207b65ac23d1f779a
GabrielBelo01 <Gabriel_itu2013@hotmail.com>
2025-09-18 21:23:13 -0300

    Commit

--- phpinfo.php
+++ phpinfo.php
@@ -25,6 +25,23 @@
                 var_dump(value: "Isso é uma string");
                 echo "<br>";
                 echo "<br>";
+                echo "Operações Aritimeticas";
+                echo "<br>";
+                echo "<br> Primeiro Calculo <br>";
+                echo 1 + 3;
+                echo "<br>";
+                echo "<br> Segundo Calculo <br>";
+                echo 2 * 3.5;
+                echo "<br>";
+                echo "<br> Terceiro Calculo <br>";
+                echo 25 / 3;
+                echo "<br>";
+                echo "<br>";
+                echo "Usando round: " . round(1234567.89123, 2);
+                echo "<br>";
+                echo "<br>";
+                echo "numner_format sem sepadores: " . number_format(1234567.89123, 2);
+                echo "<br>"; 
             ?>
         </body>
     </head>
