8ca671400fca89faa09dd321765887f8bd00cb0b
GabrielBelo01 <Gabriel_itu2013@hotmail.com>
2025-10-02 21:22:10 -0300

    Commit Aula 02/10

---
+++ ifelse.php
@@ -0,0 +1,39 @@
+<?php
+echo "Estrutura condicional if...else <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+if (true){
+    echo "Serei impresso";
+}
+
+echo "<br>";
+
+if (false){
+    echo "Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+} else {
+    echo "Falso - Parte A<br>";
+    echo "Falso - Parte B<br>";
+}
+
+if (false){
+    echo"Passo A<br>";
+} elseif (false){
+    echo"Passo B<br>";
+} elseif (false){
+    echo"Passo C<br>";
+} elseif (false){
+    echo"Passo D<br>";
+} elseif (false){
+    echo"Passo E<br>";
+} else {
+    echo"Ultimo Passo<br>";
+}
+
+
+
+
+?>
--- variaveis.php
+++ variaveis.php
@@ -66,3 +66,67 @@
     echo "<br>";
     $numero = 10;
     echo "<br> numero--: " . $numero--; // exibe o valor da variavel e depois decrementa
+    echo "<br> numero: " . $numero; // exibe o valor da variavel
+
+    echo "<br>";
+    $numero = 10;
+    echo "<br> --numero: " . $numero--; // decrementa o valor da variavel e depois exibe
+    echo "<br> --numero: " . $numero; // exibe o valor da variavel
+
+    echo "<br>";
+    $numero = 10;
+    echo "<br> numero++: " . $numero++; // exibe o valor da variavel e depois incrementa
+    echo "<br> numero: " . $numero; // exibe o valor da variavel
+
+    echo "<br>";
+    $numero = 10;
+    echo "<br> ++numero: " . ++$numero; // incrementa o valor da variavel e depois exibe
+    echo "<br> numero: " . $numero; // exibe o valor da variavel
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "Numero 20<br>";
+    $numero = 20; // atribui o valor 20 a variavel e exibe o valor
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero -= 5; // subtrai 5 do valor da variavel
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero += 5; // soma 5 ao valor da variavel
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero *= 10; // multiplica o valor da variavel por 10
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero /= 20; // divide o valor da variavel por 20
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero **= 2; // eleva o valor da variavel ao quadrado
+    echo '<br>' . $numero; // exibe o valor da variavel
+    $numero .= 4; // concatena o valor 4 ao valor da variavel
+    echo '<br>' . $numero; // exibe o valor da variavel
+
+    echo "<br>";
+    echo "<br>";
+
+    $texto = 'Esse é um texto';
+    echo '<br>' . $texto; // exibe o valor da variavel
+    $texto .= ' qualquer'; // concatena o valor ' qualquer' ao valor da variavel
+    echo '<br>' . $texto; // exibe o valor da variavel
+    $texto .= ' de verdade'; // concatena o valor ' de verdade' ao valor da variavel
+    echo '<br>' . $texto; // exibe o valor da variavel
+
+    echo "<br>";
+    echo "<br>";
+
+    echo "Constantes";
+    echo "<br>";
+    define('TAXA_DE_JUROS', 5.9); // define uma constante.
+    echo TAXA_DE_JUROS; // exibe o valor da constante.
+
+    const NOVA_TAXA = 2.5; // define uma constante.
+    echo '<br>' . NOVA_TAXA; // exibe o valor da constante.
+
+    $valorVariavel = 2.8/
+    define('NOVINHA_TAXA', $valorVariavel); // define uma constante com valor de variavel.
+    echo '<br>' . NOVINHA_TAXA; // exibe o valor da constante.
+
+    
+
+?>
