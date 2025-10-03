90a196ee8083295f8e53b13d5149ba83323d0bd4
GabrielBelo01 <Gabriel_itu2013@hotmail.com>
2025-10-02 21:31:32 -0300

    Adicionando formulario

---
+++ formulario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="#" method="post">
+        <div>
+            <label for="nome"> Nome:</label>
+            <input type="text" name="nome" value="">
+        </div>
+        <div>
+            <label for="sobrenome"> Sobrenome:</label>
+            <input type="text" name="sobrenome" value="">
+        </div>
+        <br>
+        <button type="submit" name="nomesobrenome">Executar</button>
+        <br>
+    </form>
+
+</body>
+</html>
